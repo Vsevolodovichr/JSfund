cba93661430733bc652bbb1b2953ac6d86350b69
Vsevolodovichr <130462736+Vsevolodovichr@users.noreply.github.com>
2023-07-09 13:34:04 +0000

    111

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 console.log("Шкуренко");
-// a) Оголошення двох змінних
+
 let variable1;
 let variable2;
 
-// b) Присвоєння значень змінним
 variable1 = "Значення 1";
 variable2 = "Значення 2";
 
-// c) Виведення значень змінних
 console.log(variable1);
 console.log(variable2);
 
-// d) Копіювання значення однієї змінної в іншу
 variable2 = variable1;
 
-// e) Виведення значень змінних після копіювання
 console.log(variable1);
 console.log(variable2);
+
+let login = prompt("Введіть логін:");
+let email = prompt("Введіть емейл:");
+let password = prompt("Введіть пароль:");
+
+let message = `Dear User, your email is ${email}, your password is ${password}.`;
+
+console.log(message);
